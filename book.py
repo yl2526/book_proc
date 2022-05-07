6798a0840556abe340d24c66d061279c5a5c3acc
Yi Liu <yl2526@cornell.edu>
2022-05-06 23:16:45 -0700

    move duplicated page and book processing method into processor file

--- book.py
+++ book.py
@@ -5,6 +5,8 @@ import numpy as np
 from fpdf import FPDF
 from PIL import Image
 
+from processor import processors
+
 
 class Page:
     def __init__(self, input=None, name=None, ext=None):
@@ -26,74 +28,6 @@ class Page:
             os.path.join(output, self.file_name),
             self.image
         )
-
-    def remove_border(self, top, down, left, right, skip=False):
-        if skip:
-            return self
-
-        height, width, *_ = self.image.shape
-        down = height - down
-        right = width - right
-        self.image = self.image[top:down, left:right]
-        return self
-
-    def split(self, n, edge_width=0, skip=False):
-        if skip:
-            return [self]
-
-        _, width, *_ = self.image.shape
-
-        try:
-            edge_left_width, edge_right_width = edge_width
-        except:
-            edge_left_width, edge_right_width = edge_width, edge_width
-
-
-        left_borders = [
-            int(slice / n * width + (slice != 0) * edge_right_width / 2)
-            for slice in range(n)
-        ]
-        right_borders = [
-            int(slice / n * width - (slice != n) * edge_left_width / 2)
-            for slice in range(1, n + 1)
-        ]
-        return [
-            Page(
-                self.image[:, left_border:right_border],
-                '{}-split-{}'.format(self.name, n - slice),
-                self.ext
-            )
-            for slice, (left_border, right_border)
-            in enumerate(zip(left_borders, right_borders))
-        ]
-
-    def reverse_background(self, threshold=100, skip=False):
-        if skip:
-            return self
-
-        self.image = cv2.inRange(
-            self.image,
-            np.array([0, 0, 0], dtype="uint8"),
-            np.array([threshold, threshold, threshold], dtype="uint8")
-        )
-        return self
-
-    def resize(self, factor=0.75, skip=False):
-        if skip:
-            return self
-
-        height = int(factor * self.image.shape[0])
-        width = int(factor * self.image.shape[1])
-
-        self.image = cv2.resize(self.image, (width, height), interpolation=cv2.INTER_LANCZOS4)
-        return self
-
-    def blur(self, ksize=(9, 9), sigma=(1, 1), skip=False):
-        if skip:
-            return self
-
-        self.image = cv2.GaussianBlur(self.image, ksize, *sigma)
-        return self
 
 
 class Book:
@@ -165,54 +99,7 @@ class Book:
         )
         return self
 
-    @staticmethod
-    def make_should_skip(skips):
-        return lambda page: page.name in skips or page.file_name in skips
 
-    def remove_border(self, top, down, left, right, skips={}, should_skip=None):
-        should_skip = should_skip or self.make_should_skip(skips)
-
-        self.pages = [
-            page.remove_border(
-                top, down, left, right,
-                skip=should_skip(page)
-            )
-            for page in self.pages
-        ]
-        return self
-
-    def split(self, n, edge_width=0, skips={}, should_skip=None):
-        should_skip = should_skip or self.make_should_skip(skips)
-
-        self.pages = np.hstack([
-            page.split(n, edge_width, skip=should_skip(page))
-            for page in self.pages
-        ])
-        return self
-
-    def reverse_background(self, threshold=100, skips={}, should_skip=None):
-        should_skip = should_skip or self.make_should_skip(skips)
-
-        self.pages = [
-            page.reverse_background(threshold=threshold, skip=should_skip(page))
-            for page in self.pages
-        ]
-        return self
-
-    def resize(self, factor=0.75, skips={}, should_skip=None):
-        should_skip = should_skip or self.make_should_skip(skips)
-
-        self.pages = [
-            page.resize(factor, skip=should_skip(page))
-            for page in self.pages
-        ]
-        return self
-
-    def blur(self, ksize=(9, 9), sigma=(1, 1), skips={}, should_skip=None):
-        should_skip = should_skip or self.make_should_skip(skips)
-
-        self.pages = [
-            page.blur(ksize, sigma, skip=should_skip(page))
-            for page in self.pages
-        ]
-        return self
+for name, process, book_process in processors:
+    setattr(Page, name, process)
+    setattr(Book, name, book_process)
---
+++ processor.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as np
+
+
+processors = []
+
+
+def make_book_processor(process, is_multiple):
+    def book_processor(book, *args, skips={}, should_skip=None, **options):
+        pages = []
+        for page in book.pages:
+            is_skipping = (
+                page.name in skips 
+                or page.file_name in skips
+                or (should_skip and should_skip(page))
+            )
+
+            if is_skipping:
+                 processed = page
+            else:
+                processed = process(page, *args, **options)
+            
+            if is_multiple:
+                pages.extend(processed)
+            else:
+                pages.append(processed)
+        
+        book.pages = pages
+        return book
+    return book_processor
+
+
+def registor_processor(process=None, is_multiple=False):
+    if process:
+        processors.append((process.__name__, process, make_book_processor(process, is_multiple)))
+        return process
+
+    def wrapper(process):
+        processors.append((process.__name__, process, make_book_processor(process, is_multiple)))
+        return process
+    return wrapper
+
+
+@registor_processor
+def remove_border(page, top, down, left, right):
+    height, width, *_ = page.image.shape
+    down = height - down
+    right = width - right
+    page.image = page.image[top:down, left:right]
+    return page
+
+
+@registor_processor(is_multiple=True)
+def split(page, n, edge_width=0):
+    from book import Page
+
+    _, width, *_ = page.image.shape
+
+    try:
+        edge_left_width, edge_right_width = edge_width
+    except:
+        edge_left_width, edge_right_width = edge_width, edge_width
+
+
+    left_borders = [
+        int(slice / n * width + (slice != 0) * edge_right_width / 2)
+        for slice in range(n)
+    ]
+    right_borders = [
+        int(slice / n * width - (slice != n) * edge_left_width / 2)
+        for slice in range(1, n + 1)
+    ]
+    return [
+        Page(
+            page.image[:, left_border:right_border],
+            '{}-split-{}'.format(page.name, n - slice),
+            page.ext
+        )
+        for slice, (left_border, right_border)
+        in enumerate(zip(left_borders, right_borders))
+    ]
+
+
+@registor_processor
+def reverse_background(page, threshold=100):
+
+    page.image = cv2.inRange(
+        page.image,
+        np.array([0, 0, 0], dtype="uint8"),
+        np.array([threshold, threshold, threshold], dtype="uint8")
+    )
+    return page
+
+
+@registor_processor
+def resize(page, factor=0.75):
+
+    height = int(factor * page.image.shape[0])
+    width = int(factor * page.image.shape[1])
+
+    page.image = cv2.resize(page.image, (width, height), interpolation=cv2.INTER_LANCZOS4)
+    return page
+
+
+@registor_processor
+def blur(page, ksize=(9, 9), sigma=(1, 1)):
+
+    page.image = cv2.GaussianBlur(page.image, ksize, *sigma)
+    return page
