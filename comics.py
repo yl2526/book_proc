a46320b350b1d641de88c5aa8332d31071492a85
Yi Liu <yl2526@cornell.edu>
2022-06-18 22:49:40 -0700

    handle renaming for opencv

--- comics.py
+++ comics.py
@@ -7,18 +7,40 @@ if not os.path.isdir(out):
     os.mkdir(out)
 
 
-
-
-
 folder = r'C:\BaiduYunDownload\漫画\temp'
+# for comic in os.listdir(folder):
+#     try:
+#         print(f'Processing {comic}')
+#         book = Book(os.path.join(folder, comic))
+#         book.to_pdf(out)
+#     except Exception as e:
+#         import ipdb; ipdb.set_trace()
+
+
+
+current_dir = os.getcwd()
+os.chdir(folder)
 for comic in os.listdir(folder):
     try:
         print(f'Processing {comic}')
-        book = Book(os.path.join(folder, comic))
-        book.to_pdf(out)
+        
+        with open('name_backup.txt', 'w+', encoding='utf-8') as f:
+            f.write(comic)
+        os.rename(comic, 'processing')
+        book = (
+            Book('processing')
+            .blur((3, 3))
+        )
+        book.to_pdf(out, comic)
     except Exception as e:
-        import ipdb; ipdb.set_trace()
-
+        os.rename('processing', comic)
+        print(f'Processing {comic} failed')
+        print(e)
+        # import ipdb; ipdb.set_trace()
+    finally:
+        os.rename('processing', comic)
+os.remove('name_backup.txt')
+os.chdir(current_dir)
 
 
 # comic = r'[[momi] ふらっぴー]'
